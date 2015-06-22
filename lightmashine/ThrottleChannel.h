22b3d5696da03e3b9712672015555bdf965df166
BruegelN <BruegelN@users.noreply.github.com>
2015-06-22 22:23:37 +0200

    Fixed comment for serviceRoutine.

--- lightmashine/ThrottleChannel.h
+++ lightmashine/ThrottleChannel.h
@@ -21,7 +21,8 @@ class ThrottleChannel {
     private:
 		  static ThrottleChannel* ownerChannel;
       /*
-      __vector_4 is just the interrupt vector for analoge pins
+      __vector_4 is just the interrupt vector for analoge pins on Atmega 328p
+      If you try attach the interrupt to other pins you might have an unhandled interrupt which causes a reset.
       TODO map pins to interrupt verctors !
       */
 		  static void serviceRoutine() __asm__("__vector_4") __attribute__((__signal__, __used__, __externally_visible__));
