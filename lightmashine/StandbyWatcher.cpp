7473b2ff424d7baf8ead3dd66ead9979edf6e983
BruegelN <BruegelN@users.noreply.github.com>
2016-03-15 23:41:22 +0100

    Added a few comments in StandbyWatcher.cpp.

--- lightmashine/StandbyWatcher.cpp
+++ lightmashine/StandbyWatcher.cpp
@@ -17,11 +17,11 @@ void StandbyWatcher::update(int8_t throttleValue)
   {
     // if the throttle value is not equal to zero we're obviously not moving
     _lastTimeMoved = millis();
-
   }
 
   if((millis()-_lastTimeMoved) >= _standbyTimeOut)
   {
+    // enter standby-mode
     if(!_standby)
     {
       // if haven't been in standy-mode before
@@ -31,19 +31,24 @@ void StandbyWatcher::update(int8_t throttleValue)
     }
     else
     {
+      // are already in standby-mode, thus state has not changed!
       _hasChanged = false;
     }
 
   }
   else
   {
+    // we're not in standby-mode
     if(_standby)
     {
+      // previously been in standby-mode
+      // so set the _hasChanged and reset/disable _standby
       _standby = false;
       _hasChanged = true;
     }
     else
     {
+      // haven't been in standby-mode before, thus state has not changed!
       _hasChanged = false;
     }
   }
